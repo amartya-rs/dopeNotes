dd4a559a72a50bd368c5cd30a52941d3763b1cec
amartya-rs <saha.amartya@outlook.com>
2022-03-29 18:02:40 +0530

    initialized with mockbee

---
+++ src/backend/db/users.js
@@ -0,0 +1,27 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * */
+
+export const users = [
+   {
+      _id: uuid(),
+      firstName: "Adarsh",
+      lastName: "Balika",
+      email: "adarshbalika@gmail.com",
+      password: "adarshBalika123",
+      createdAt: formatDate(),
+      updatedAt: formatDate(),
+   },
+   {
+      _id: uuid(),
+      firstName: "test",
+      lastName: "admin",
+      email: "test@admin.com",
+      password: "admin123",
+      createdAt: formatDate(),
+      updatedAt: formatDate(),
+   },
+];
