013e8712faac85bb4929cdbb123258640a2b09d7
amartya-rs <saha.amartya@outlook.com>
2022-03-31 23:50:02 +0530

    feat: note card component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,17 @@
 import { TopNav } from "./top-navigation/TopNav";
 import { Footer } from "./footer/Footer";
 import { Sidebar } from "../components/sidebar/Sidebar";
+import { CreateNoteCard } from "./create-note-card/CreateNoteCard";
+import { NoteCard } from "./note-card/NoteCard";
+import { ColorPalette } from "./color-pallete/ColorPalette";
+import { TagInput } from "./tag-input/TagInput";
 
-export { TopNav, Footer, Sidebar };
+export {
+   TopNav,
+   Footer,
+   Sidebar,
+   CreateNoteCard,
+   NoteCard,
+   ColorPalette,
+   TagInput,
+};
