32d609c55b3d7e7388fc7c2a32ebe2e6199fa890
amartya-rs <saha.amartya@outlook.com>
2022-03-31 23:58:15 +0530

    refactor: props

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -157,12 +157,12 @@ const UserIcon = () => {
    );
 };
 
-const CrossIcon = () => {
+const CrossIcon = ({ width, height, handler }) => {
    return (
       <svg
          xmlns="http://www.w3.org/2000/svg"
-         width="24"
-         height="24"
+         width={width ?? "24"}
+         height={height ?? "24"}
          viewBox="0 0 24 24"
          fill="none"
          stroke="currentColor"
@@ -170,6 +170,7 @@ const CrossIcon = () => {
          strokeLinecap="round"
          strokeLinejoin="round"
          className="feather feather-x"
+         onClick={handler}
       >
          <line x1="18" y1="6" x2="6" y2="18" />
          <line x1="6" y1="6" x2="18" y2="18" />
