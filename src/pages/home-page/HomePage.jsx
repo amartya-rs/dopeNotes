f96b6168d4ff0dfc399d15fe4e4e0dc272fb5be0
amartya-rs <saha.amartya@outlook.com>
2022-03-31 23:56:33 +0530

    refactor: added note cards

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,15 +1,28 @@
-import { Sidebar } from "../../components/index";
+import { Sidebar, NoteCard, CreateNoteCard } from "../../components/index";
+import { useNote } from "../../context/note-context";
 import { useLocation } from "react-router-dom";
 import "../page.css";
 
 const HomePage = () => {
    const { pathname } = useLocation();
+   const { state, dispatch } = useNote();
 
    return (
       <div className="page">
          <Sidebar activePage={pathname} />
          <main className="mt-2">
-            <h5>Home</h5>
+            <button
+               className="button primary mb-2"
+               onClick={() => dispatch({ type: "TOGGLE_CARD_VISIBILITY" })}
+            >
+               {state.isVisible ? "CLOSE" : "CREATE NEW NOTE"}
+            </button>
+            <CreateNoteCard />
+            <section className="mt-4">
+               {state.allNotes.map((item) => (
+                  <NoteCard key={item._id} note={item} />
+               ))}
+            </section>
          </main>
       </div>
    );
