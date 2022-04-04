87537206c3ffba38c122f75b267c192e11d3b58c
amartya-rs <saha.amartya@outlook.com>
2022-04-04 10:32:05 +0530

    fix: missing color

--- src/utils/generateColorClassArray .js
+++ src/utils/generateColorClassArray .js
@@ -1,6 +1,6 @@
 const generateColorClassArray = (noOfColor) => {
    const ColorClassArray = [];
-   for (let i = 0; i < noOfColor; i++) {
+   for (let i = 1; i <= noOfColor; i++) {
       ColorClassArray.push(`color-${i}`);
    }
    return ColorClassArray;
