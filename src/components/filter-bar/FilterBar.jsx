1973dac883aef87011a77736d00e358baee12a1a
amartya-rs <saha.amartya@outlook.com>
2022-04-04 18:32:57 +0530

    feat: filter bar component

---
+++ src/components/filter-bar/FilterBar.jsx
@@ -0,0 +1,32 @@
+import { allTagsArray } from "../../utils/allTagsArray";
+import { useNote } from "../../context/note-context";
+import "./filter-bar.css";
+
+const FilterBar = () => {
+   const { state, dispatch } = useNote();
+
+   return (
+      <div className="filter-bar">
+         <label htmlFor="filter">Filter by tag</label>
+         <select
+            onChange={(e) =>
+               dispatch({
+                  type: "FILTER_BY_TAG",
+                  payload: e.target.value,
+               })
+            }
+            id="filter"
+            value={state.filterByTag}
+         >
+            <option value="">select a tag</option>
+            {allTagsArray(state).map((item, index) => (
+               <option key={index} value={item}>
+                  {item}
+               </option>
+            ))}
+         </select>
+      </div>
+   );
+};
+
+export { FilterBar };
